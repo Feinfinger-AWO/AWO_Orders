8f6ebbffbca05e6c902542c2c1b113f7dd469a16
feinfinger <l.tepper@gmx.de>
2020-08-05 16:12:01 +0200

    Add Search and Paging to extern orders

---
+++ Components/PaginatedList.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWO_Orders.Components
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(
+            IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            List<T> items = null;
+            var count = 0;
+
+            if (source.Any())
+            {
+                count  = await source.CountAsync();
+
+                items = await source.Skip(
+                    (pageIndex - 1) * pageSize)
+                    .Take(pageSize).ToListAsync();
+            }
+            else
+            {
+                 items = new List<T>();
+            }
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
--- Pages/ExternalOrders/Index.cshtml.cs
+++ Pages/ExternalOrders/Index.cshtml.cs
@@ -7,25 +7,42 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using AWO_Orders.Data;
 using AWO_Orders.Models;
+using AWO_Orders.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace AWO_Orders.Pages.ExternalOrders
 {
     public class IndexModel : BasePageModel
     {
         private readonly AWO_Orders.Data.ExternalOrdersContext _context;
+        private string filterText;
 
         public IndexModel(AWO_Orders.Data.ExternalOrdersContext context)
         {
             _context = context;
         }
 
-        public IList<ExternalOrderModel> ExternalOrderModel { get;set; }
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? pageIndex,string searchString)
         {
-            ExternalOrderModel = await _context.ExternalOrders
-                .Include(e => e.Employee)
-                .Include(e => e.Manager).ToListAsync();
+            filterText = searchString;
+            var models = (!String.IsNullOrWhiteSpace(searchString)) ? from e in _context.ExternalOrders
+                                                                      where e.Manager.SureName.ToLower().Contains(searchString.ToLower()) ||
+                                                                             e.Notes.ToLower().Contains(searchString.ToLower())
+                                                                      select e :
+                                                                        _context.ExternalOrders;
+            if (models.Any())
+            {
+                ExternalOrderModel = await PaginatedList<ExternalOrderModel>.CreateAsync(
+                                             models.Include(e => e.Employee)
+                    .Include(e => e.Manager).AsNoTracking(), pageIndex ?? 1, 10);
+            }
+            else
+            {
+                ExternalOrderModel = await PaginatedList<ExternalOrderModel>.CreateAsync(models.AsNoTracking(), 1, 0);
+            }
         }
+
+        public PaginatedList<ExternalOrderModel> ExternalOrderModel { get; set; }
+        public string FilterText { get => filterText; set => filterText = value; }
     }
 }
