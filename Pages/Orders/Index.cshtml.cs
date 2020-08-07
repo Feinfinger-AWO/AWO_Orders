ab5646f546aaea9b6f135bba12e8134e467b07c9
feinfinger <l.tepper@gmx.de>
2020-08-07 12:58:37 +0200

    Added Paging to orders

--- Pages/Orders/Index.cshtml.cs
+++ Pages/Orders/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using AWO_Orders.Data;
 using AWO_Orders.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using AWO_Orders.Components;
 
 namespace AWO_Orders.Pages.Orders
 {
@@ -17,6 +18,7 @@ namespace AWO_Orders.Pages.Orders
         private readonly OrderStatusContext _orderStatusContext;
         private string filterText;
         private int filterStatusId;
+        private bool pagingEnabled;
 
         public IndexModel(AWO_Orders.Data.OrdersContext context, OrderStatusContext orderStatusContext)
         {
@@ -24,11 +26,11 @@ namespace AWO_Orders.Pages.Orders
             _orderStatusContext = orderStatusContext;
         }
 
-        public async Task OnGetAsync(string searchString, int? filterStatusId,int? id)
+        public async Task OnGetAsync(string searchString, int? filterStatusId, int? id,int? pageIndex)
         {
             var loginItem = GetLogin();
 
-            FilterStatusId = filterStatusId?? 1;
+            FilterStatusId = filterStatusId ?? 1;
             FilterText = searchString;
 
             if (id.HasValue)
@@ -36,20 +38,47 @@ namespace AWO_Orders.Pages.Orders
                 await SetReady(id.Value);
             }
 
-            var orders = (loginItem.Right.CanProcess) ?
-                    from s in _context.Orders where s.StatusId == FilterStatusId select s :
-                      from s in _context.Orders where s.StatusId == FilterStatusId && s.EmplId == loginItem.EmployeeId select s;
-
-            OrderModel = await orders
-                .Include(o => o.Employee)
-                .Include(o => o.Status).ToListAsync();
+            IQueryable<OrderModel> orders = null;
 
             if (!String.IsNullOrWhiteSpace(FilterText))
             {
-                OrderModel = OrderModel.Where(a => a.Employee.SureName.ToLower().Contains(FilterText.ToLower()) || a.Number.ToLower().Contains(FilterText.ToLower())).ToList();
+
+                orders = (loginItem.Right.CanProcess) ?
+                        from s in _context.Orders where s.StatusId == FilterStatusId &&
+                        (s.Employee.SureName.ToLower().Contains(FilterText.ToLower()) || s.Number.ToLower().Contains(FilterText.ToLower()))
+                        select s :
+                        from s in _context.Orders where s.StatusId == FilterStatusId && s.EmplId == loginItem.EmployeeId &&
+                                                (s.Employee.SureName.ToLower().Contains(FilterText.ToLower()) || s.Number.ToLower().Contains(FilterText.ToLower()))
+                        select s;
+            }
+            else
+            {
+                orders = (loginItem.Right.CanProcess) ?
+                        from s in _context.Orders where s.StatusId == FilterStatusId select s :
+                          from s in _context.Orders where s.StatusId == FilterStatusId && s.EmplId == loginItem.EmployeeId select s;
             }
 
-            OrderModel = OrderModel.OrderBy(a => a.PlaceDate).ToList();
+            if (PagingEnabled)
+            {
+                if (orders.Any())
+                {
+                    POrderModel = await PaginatedList<OrderModel>.CreateAsync(
+                        orders.Include(o => o.Employee).Include(o => o.Status)
+                        .AsNoTracking(), pageIndex ?? 1, 10);
+                }
+                else
+                {
+                    POrderModel = await PaginatedList<OrderModel>.CreateAsync(orders.AsNoTracking(), 1, 0);
+                }
+            }
+            else
+            {
+                OrderModel = await orders
+                    .Include(o => o.Employee)
+                     .Include(o => o.Status).ToListAsync();
+                OrderModel = OrderModel.OrderBy(a => a.PlaceDate).ToList();
+            }
+
             ViewData["StatusId"] = new SelectList(_context.Set<OrderStatusModel>(), "Id", "Ident");
         }
 
@@ -65,5 +94,19 @@ namespace AWO_Orders.Pages.Orders
 
         public IList<OrderModel> OrderModel { get; set; }
         public int FilterStatusId { get => filterStatusId; set => filterStatusId = value; }
+        public PaginatedList<OrderModel> POrderModel { get; set; }
+        public bool PagingEnabled 
+        { 
+            get
+            {
+                var status = (from s in _context.OrderStatus where s.Id == FilterStatusId select s).First();
+                pagingEnabled = status.BaseStatus == OrderBaseStatusEnum.Canceled || status.BaseStatus == OrderBaseStatusEnum.Delivered;
+                return pagingEnabled;
+            }
+            set 
+            { 
+                pagingEnabled = value; 
+            } 
+        }
     }
 }
