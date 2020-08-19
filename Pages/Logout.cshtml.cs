9f2189501590a15f6d7dbd857e483c60fadcba36
feinfinger <l.tepper@gmx.de>
2020-08-19 15:54:38 +0200

    Logout page added

---
+++ Pages/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWO_Orders.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWO_Orders.Pages
+{
+    public class LogoutModel : BasePageModel
+    {
+        public void OnGet()
+        {
+            Logout();
+        }
+    }
+}
